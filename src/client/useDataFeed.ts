8f13dd6a0b2a79f8a9eaa57424f69ba99f6c8b25
David W J Reid <reid5587@gmail.com>
2021-10-20 21:13:35 +0100

    Requirement 1: add websocket subscription
    Todo:
    1. Add more Types
    2. Error Handling

---
+++ src/client/useDataFeed.ts
@@ -0,0 +1,51 @@
+import { useCallback, useEffect, useRef } from "react";
+import useStore from "../store";
+import type { OrderTuple } from "../store";
+
+const ENDPOINT = "wss://www.cryptofacilities.com/ws/v1";
+
+type MessageEvent = {
+  feed: string;
+  product_id: string;
+  bids: OrderTuple[];
+  asks: OrderTuple[];
+};
+
+export default function useDataFeed(product: string = "PI_XBTUSD") {
+  const setInitialSnapshot = useStore((state) => state.setInitialSnapshot);
+  const webSocketRef = useRef<WebSocket>();
+
+  const handleOpen = useCallback(() => {
+    const subscriptionMessage = JSON.stringify({
+      event: "subscribe",
+      feed: "book_ui_1",
+      product_ids: [product],
+    });
+
+    if (webSocketRef.current?.OPEN) {
+      webSocketRef.current?.send(subscriptionMessage);
+    }
+  }, [product]);
+
+  useEffect(() => {
+    webSocketRef.current = new WebSocket(ENDPOINT);
+    webSocketRef.current.onopen = () => handleOpen();
+
+    webSocketRef.current.onclose = () => console.log("ws closed");
+
+    webSocketRef.current.onmessage = (msg) => {
+      const message: MessageEvent = JSON.parse(msg.data);
+      //console.log(message);
+
+      if (message?.feed?.toLowerCase().includes("snapshot")) {
+        setInitialSnapshot({ bids: message.bids, asks: message.asks });
+      }
+    };
+
+    return () => {
+      if (webSocketRef.current) {
+        webSocketRef.current.close();
+      }
+    };
+  }, [handleOpen, setInitialSnapshot]);
+}
