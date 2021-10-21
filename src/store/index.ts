6bdaf0c914d87f69cf72193980ffc45a0af1117e
David W J Reid <reid5587@gmail.com>
2021-10-21 15:27:35 +0100

    Requirement 1: move processing functions to another file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import create from "zustand";
+import { calculateTotalsAtLevel, handleDelta } from "./processing";
 
 export type OrderTuple = [price: number, size: number, total?: number];
 
@@ -17,66 +18,17 @@ type Delta = {
 interface GlobalAppState {
   bids?: OrderTuple[];
   asks?: OrderTuple[];
-  numLevels?: number;
+  numLevels: number;
   productId?: string;
   // setters
   setInitialSnapshot: (source: InitialSnapshotSource) => void;
   processDelta: (source: Delta) => void;
 }
 
-function calculateTotalsAtLevel(orders: OrderTuple[], startLevel = 0) {
-  let runningTotal = 0;
-
-  for (let i = startLevel; i <= orders.length - 1; i++) {
-    let order = orders[i];
-    runningTotal += order[1];
-    order[2] = runningTotal;
-  }
-
-  return orders;
-}
-
-//[64965, 0]
-// 1: (2) [64965.5, 2900]
-
-function handleDelta(currentOrders: OrderTuple[], deltas: OrderTuple[]) {
-  let deltaSize: number;
-  let deltaPrice: number;
-
-  const nextOrders = [...currentOrders];
-
-  deltas.forEach((delta) => {
-    deltaPrice = delta[0];
-    deltaSize = delta[1];
-
-    const orderIndex = nextOrders.findIndex((order) => order[0] === deltaPrice);
-
-    if (orderIndex >= 0) {
-      // updating an existing order
-      if (deltaSize === 0) {
-        //remove from order book
-        console.log("removing", delta);
-        nextOrders.splice(orderIndex, 1);
-      } else {
-        //update price
-        nextOrders[orderIndex][1] = delta[1];
-      }
-    } else {
-      //brand new order
-      if (deltaSize > 0) {
-        nextOrders.push(delta);
-      }
-    }
-  });
-
-  console.log(currentOrders.length);
-  return calculateTotalsAtLevel(nextOrders);
-}
-
 const useStore = create<GlobalAppState>((set, get) => ({
   bids: [],
   asks: [],
-  numLevels: 10,
+  numLevels: 25,
   setInitialSnapshot: ({ bids, asks, numLevels, productId }) =>
     set({
       bids: calculateTotalsAtLevel(bids),
@@ -86,8 +38,8 @@ const useStore = create<GlobalAppState>((set, get) => ({
     }),
   processDelta: ({ deltaBids, deltaAsks }) =>
     set({
-      bids: handleDelta(get().bids || [], deltaBids || []),
-      asks: handleDelta(get().asks || [], deltaAsks || []),
+      bids: handleDelta(get().bids || [], deltaBids || [], get().numLevels),
+      asks: handleDelta(get().asks || [], deltaAsks || [], get().numLevels),
     }),
 }));
 
---
+++ src/store/processing.ts
@@ -0,0 +1,66 @@
+import { OrderTuple } from "./index";
+
+const priceComparator = (a: OrderTuple, b: OrderTuple) => {
+  const aPrice = a[0];
+  const bPrice = b[0];
+
+  if (aPrice < bPrice) {
+    return -1;
+  }
+  if (aPrice > bPrice) {
+    return 1;
+  }
+  return 0;
+};
+
+export function calculateTotalsAtLevel(orders: OrderTuple[], startLevel = 0) {
+  let runningTotal = 0;
+
+  for (let i = startLevel; i <= orders.length - 1; i++) {
+    let order = orders[i];
+    runningTotal += order[1];
+    order[2] = runningTotal;
+  }
+
+  return orders;
+}
+
+export function handleDelta(
+  currentOrders: OrderTuple[],
+  deltas: OrderTuple[],
+  numLevels: number
+) {
+  let deltaSize: number;
+  let deltaPrice: number;
+
+  const nextOrders = [...currentOrders];
+
+  deltas.forEach((delta) => {
+    deltaPrice = delta[0];
+    deltaSize = delta[1];
+
+    const orderIndex = nextOrders.findIndex((order) => order[0] === deltaPrice);
+
+    if (orderIndex >= 0) {
+      // updating an existing order
+      if (deltaSize === 0) {
+        //remove from order book
+        nextOrders.splice(orderIndex, 1);
+      } else {
+        //update price
+        nextOrders[orderIndex][1] = delta[1];
+      }
+    } else {
+      //brand new order - sanity check on it's size
+      if (deltaSize > 0) {
+        nextOrders.push(delta);
+      }
+    }
+  });
+
+  // because new orders are pushed in at the end
+  // we need to sort by price to ensure correct total calculations
+  return calculateTotalsAtLevel(
+    nextOrders.sort(priceComparator).slice(0, numLevels)
+  );
+}
